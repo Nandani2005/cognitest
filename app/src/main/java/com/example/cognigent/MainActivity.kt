ead34a419139e020ec3cfb9eb157cd9b84b68183
[nandani mahato] <[nandanimahato73@gmail.com]>
2025-06-28 23:45:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cognigent/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.cognigent
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.cognigent.ui.theme.CognigentTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.logo)
+        lifecycleScope.launch {
+            delay(1500) // 5000 milliseconds = 5 seconds
+            // Move to next activity
+            val trans= Intent(this@MainActivity, testPageActivity::class.java)
+            startActivity(trans)
+            finish()
+        }
+    }
+}
