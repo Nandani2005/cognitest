a59c74e7833aa89c08897a28e3175e801810ac05
[nandani mahato] <[nandanimahato73@gmail.com]>
2025-06-29 00:09:06 +0530

    new layout added

--- app/src/main/java/com/example/cognigent/testPageActivity.kt
+++ app/src/main/java/com/example/cognigent/testPageActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cognigent
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +11,28 @@ import androidx.core.view.WindowInsetsCompat
 class testPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.test_page)
 
+        // Reference to ListView
+        val listView = findViewById<ListView>(R.id.list)
+        // Data to show in the ListView
+        val dataList = listOf(
+            "Math Test",
+            "Science Quiz",
+            "English Exam",
+            "History Paper",
+            "Geography Assignment",
+            "Coding Challenge"
+        )
+
+        // Create adapter
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            dataList
+        )
+
+        // Set adapter to ListView
+        listView.adapter = adapter
     }
 }
